e6e997b44dae40a998dd9ef7783999a7aed39299
JImumu <jimumu8@163.com>
2020-07-13 19:04:02 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,40 +2,37 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-Vue.config.productionTip = false
+import NProgress from 'nprogress'
 
 router.beforeEach((to, from, next) => {
 
-  // 获取下一个路由的query
-  const routeQuery = JSON.stringify(to.query)
-  // 获取vuex中下一个路由的query（）
-  const routeQueryInRecord = store.state.keepAlive.record[to.name + ':routeQuery']
-  // 准备vuex仓库里keepAlive的数据
-  const data = {
-    name: to.name,
-    value: routeQuery
-  }
-  // 获取下一个路由的父路由
-  const parentRouteArr = to.matched.map(e => e.parent ? e.parent.name : '')
-  // vuex中的记录与获取下一个路由的query不一致，则表示页面需要重新创建（从keep-alive组件的include属性中移除）
-  if (routeQueryInRecord && routeQuery !== routeQueryInRecord) {
-    // 通过vuex，改变keep-alive组件的include属性
-    store.dispatch('keepAlive/remove', to.name)
-    // 父路由需要一起删除
-    parentRouteArr.forEach(e => e && store.dispatch('keepAlive/remove', e))
+  if (!to.meta.notKeepAlive) {
+    const fullPath = to.fullPath
+    const fullPathInRecord = store.state.keepAlive.record[to.name + ':fullPath']
+    const data = {
+      name: to.name,
+      value: fullPath
+    }
+    const parentRouteArr = to.matched.map(e => e.parent ? e.parent.name : '')
+    // vuex中的记录与获取下一个路由的query不一致，则表示页面需要重新创建（从keep-alive组件的include属性中移除）
+    if (fullPathInRecord && fullPath !== fullPathInRecord) {
+      NProgress.start()
+      // 通过vuex，改变keep-alive组件的include属性
+      store.dispatch('keepAlive/remove', to.name)
+      // 父路由的记录需要一起改变
+      parentRouteArr.map(e => e && store.dispatch('keepAlive/remove', e))
+    } else {
+      store.dispatch('keepAlive/add', data)
+      parentRouteArr.map(e => e && store.dispatch('keepAlive/add', e))
+    }
+    setTimeout(() => {
+      // next()之后记录这次的路由
+      store.dispatch('keepAlive/add', data)
+      parentRouteArr.map(e => e && store.dispatch('keepAlive/add', e))
+    }, 0)
   } else {
-    // vuex中的记录与获取下一个路由的query一致，则表示页面需要被缓存（向keep-alive组件的include属性里添加）
-    // 通过vuex，改变keep-alive组件的include属性
-    store.dispatch('keepAlive/add', data)
-    // 父路由需要一起增加
-    parentRouteArr.forEach(e => e && store.dispatch('keepAlive/add', e))
+    NProgress.start()
   }
-  setTimeout(() => {
-    // 进入页面后记录这次的路由
-    store.dispatch('keepAlive/add', data)
-    parentRouteArr.forEach(e => e && store.dispatch('keepAlive/add', e))
-  }, 0)
   next()
 })
 
